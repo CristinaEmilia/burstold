6cf3015b5bfa4d08053ccfc06727cd8175a1e543
cristinaemilia <budacristina9@gmail.com>
2017-05-09 00:29:01 -0400

    buffer overwrite issues

--- burst.c
+++ burst.c
@@ -4,6 +4,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -13,51 +14,93 @@
 #define BLOCK 2048
 
 
+
+
 int main(int argc, char *argv[])
 {
-    int infd = STDIN_FILENO;
+    int readfd = STDIN_FILENO;
 
     // open a file
     if (argc > 1)
     {
-        infd = open(argv[1], O_RDONLY);
+        readfd = open(argv[1], O_RDONLY);
     }
 
-    int fcount = 0;
-    int linecount = 0;
-    char fname[512];
-    snprintf(fname, 512, "%s.%d", argv[1], ++fcount);
-    int writefd = open(fname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    int fileCount = 0;
+    int lineCount = 0;
+    char fileName[512];
+   
     // read the opened file
-    char buf[BLOCK];
+    char readBuffer[BLOCK];
+    int lineBufferLen = BLOCK;
+    int lineBufferIndex = 0;
+    char* lineBuffer = (char*)malloc(BLOCK);
+   
 
-    int bytesread = 0;
-    while ((bytesread = read(infd, buf, BLOCK)) > 0)
+    int bytesRead = 0;
+    while ((bytesRead = read(readfd, readBuffer, BLOCK)) > 0)
     {
-        int newLineIndex = 0;
-
-        for (int i = 0; i < bytesread; i++)
+        if(lineBufferIndex + bytesRead > lineBufferLen)
+        { 
+            lineBuffer = (char*) realloc(lineBuffer,lineBufferLen + BLOCK);
+            printf("EXTENDING BUFFER FROM %d to %d\n",lineBufferLen,lineBufferLen + BLOCK);
+            lineBufferLen = lineBufferLen + BLOCK; 
+            
+        }
+        //copy the bytes read to the current readBufferfer
+        memcpy(lineBuffer+lineBufferIndex,readBuffer,bytesRead);
+        lineBufferIndex += bytesRead;
+        printf("read from %d to %d %d bytes\n",lineBufferIndex - bytesRead, lineBufferIndex,bytesRead);
+        
+        int i;
+        for (i = 0; i < bytesRead; i++)
         {
-            if (buf[i] == '\n')
+            if (readBuffer[i] == '\n' && ++lineCount >= 500)
             {
-                if (++linecount >= 500)
+                lineCount = 0;
+                int pid = fork();
+                if(pid < 0)
                 {
-                    linecount = 0;
-                    write(writefd, buf + newLineIndex, i);
-                    snprintf(fname, 512, "%s.%d", argv[1], ++fcount);
+                    fprintf(stderr, "ERROR: unable to fork\n");
+                    return 1;
+                }
+
+                snprintf(fileName, 512, "%s.%d", argv[1], ++fileCount);
+                int lineBufferOffset = (BLOCK - i);
+                if(pid > 0) {  
+                    //parent
+                   
+                    //set the line buffer index to 0, but keep using same line buffer
+                    memcpy(lineBuffer,lineBuffer + lineBufferIndex - lineBufferOffset + 1, i + 1);
+                    printf("lineBufferIndex: %d - index: %d - offset: %d - bytesread: %d - writing total: %d\n",lineBufferIndex,i,lineBufferOffset,bytesRead,lineBufferIndex - lineBufferOffset + 1);
+                    lineBufferIndex = i + 1;
+                    memset(lineBuffer + i + 1,0,lineBufferLen);
+                    if(fileCount == 2)
+                        return;
+                    break;
+                } else { 
+                    //child
+                    int writefd = open(fileName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+                    write(writefd,lineBuffer, lineBufferIndex - lineBufferOffset + 1);
                     close(writefd);
-                    writefd = open(fname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-                    newLineIndex = i;
-                    continue;
+                    return 0;
                 }
             }
         }
-
-        write(writefd,buf+newLineIndex,bytesread - newLineIndex);
     }
-    
-    close(writefd);
-    close(infd);
+
+    if(lineBufferIndex > 0) {
+        snprintf(fileName, 512, "%s.%d", argv[1], ++fileCount);
+        if(fork() == 0)
+        {
+            int writefd = open(fileName, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+            write(writefd,lineBuffer, lineBufferIndex);
+            close(writefd);
+            return 0;
+        }
+    }
+
+    close(readfd);
 
     return 0;
 }
